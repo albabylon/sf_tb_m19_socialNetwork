1ff4d5d4e9c8648004ada529426a68c3b01f0014
aveA <aveA@AVE>
2025-03-02 12:04:15 +0300

    registartion

---
+++ SocialNetwork/SocialNetwork/BLL/Services/UserService.cs
@@ -0,0 +1,54 @@
+﻿using SocialNetwork.BLL.Models;
+using SocialNetwork.DAL.Entities;
+using SocialNetwork.DAL.Repositories;
+using System.ComponentModel.DataAnnotations;
+
+namespace SocialNetwork.BLL.Services
+{
+    public class UserService
+    {
+        IUserRepository userRepository;
+        
+        public UserService()
+        {
+            userRepository = new UserRepository();
+        }
+
+        public void Register(UserRegistrationData userRegistrationData)
+        {
+            // проверка введенных данных
+            if (String.IsNullOrEmpty(userRegistrationData.FirstName))
+                throw new ArgumentNullException();
+
+            if (String.IsNullOrEmpty(userRegistrationData.LastName))
+                throw new ArgumentNullException();
+
+            if (String.IsNullOrEmpty(userRegistrationData.Password))
+                throw new ArgumentNullException();
+
+            if (String.IsNullOrEmpty(userRegistrationData.Email))
+                throw new ArgumentNullException();
+
+            if (userRegistrationData.Password.Length < 8)
+                throw new ArgumentNullException();
+
+            if (!new EmailAddressAttribute().IsValid(userRegistrationData.Email))
+                throw new ArgumentNullException();
+
+            if (userRepository.FindByEmail(userRegistrationData.Email) != null)
+                throw new ArgumentNullException();
+
+            // сохдание сущности юзера и отправка в бд
+            var userEntity = new UserEntity()
+            {
+                firstname = userRegistrationData.FirstName,
+                lastname = userRegistrationData.LastName,
+                password = userRegistrationData.Password,
+                email = userRegistrationData.Email
+            };
+
+            if (this.userRepository.Create(userEntity) == 0)
+                throw new Exception();
+        }
+    }
+}
